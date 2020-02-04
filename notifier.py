82d67b291e4d54d266f404df612acbf7c7fbbdc8
root <you@example.com>
2020-02-04 18:32:39 +0100

    second

--- notifier.py
+++ notifier.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python3
-
 # requirements 
 
 import gi
@@ -7,29 +5,14 @@ gi.require_version('Notify', '0.7')
 from gi.repository import Notify
 Notify.init("Test Notifier")
 
-#def main(): 
- #  notification = Notify.Notification.new(
-  #         "Hey!", "\nHow are you doing ?",
-   #     "face-smile" ,  
-           #  )
-#   notification.show() 
+def main(): 
+  notification = Notify.Notification.new(
+      "Hey!", "\nHow are you doing ?",
+   "face-smile" ,  
+  )
+   notification.show() 
 
 
-#def status(): 
-     
-
-
-def notify(self, msg, title="Ava Message"):
-    if self.notification is None:
-        self.notification = Notify.Notification.new(
-            title,
-            msg,
-            resource_path("res/icon.png"))
-        self.notification.set_app_name("EAvatar")
-    else:
-        self.notification.update(title, msg)
-    self.notification.set_timeout(3)
-    self.notification.show()
 
 
 

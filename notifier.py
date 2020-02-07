e68d0373708e8c53f7c8c92b660545611480c3e8
root <root@kali.KMx404>
2020-02-07 03:47:45 -0500

    new

--- notifier.py
+++ notifier.py
@@ -5,7 +5,7 @@ import gi
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify
 Notify.init("Test Notifier")
-
+# new comment 
 def main(): 
   notification = Notify.Notification.new(
       "Hey!", "\nHow are you doing ?",
